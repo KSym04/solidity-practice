b294d4f1d37a19d9e3bb597e8df9a5eb80ca867f
ksym04 <dev@dopethemes.com>
2022-01-19 12:24:42 +0800

    Added all variables practice session.

--- Variables.sol
+++ Variables.sol
@@ -3,9 +3,46 @@
 pragma solidity ^0.8.7;
 
 contract Variables {
-    uint public myUint;
+    // Uint 256
+    uint256 public myUint;
 
     function setMyUint(uint _myUint) public {
         myUint = _myUint;
+    }
+
+    // Boolean
+    bool public myBool;
+
+    function setMyBool(bool _myBool) public {
+        myBool = _myBool;
+    }
+
+    // Uint8
+    uint8 public myUint8;
+
+    function incrementUint() public {
+        myUint8++;
+    }
+
+    function decrementUint() public {
+        myUint8--;
+    }
+
+    // Addresses
+    address public myAddress;
+
+    function setMyAddress(address _myAddress) public {
+        myAddress = _myAddress;
+    }
+
+    function getBalanceOfAddress() public view returns(uint) {
+        return myAddress.balance;
+    }
+
+    // Strings
+    string public myString = "PunkiesVerse";
+
+    function setMyString(string memory _myString) public {
+        myString = _myString;
     }
 }
