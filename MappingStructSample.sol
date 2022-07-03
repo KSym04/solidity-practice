54b3dd0d223bddb2747923dec79b63a88838c2c2
ksym04 <dev@dopethemes.com>
2022-07-03 23:05:48 +0800

    Minor changes.

--- MappingStructSample.sol
+++ MappingStructSample.sol
@@ -2,13 +2,25 @@
 pragma solidity 0.8.1;
 
 contract MappingStructSample {
+    mapping(address => uint256) public balanceReceived;
+
     function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
 
-    function sendMoney() public payable {}
+    function sendMoney() public payable {
+        balanceReceived[msg.sender] += msg.value;
+    }
+
+    function withdrawMoney(address payable _to, uint256 _amount) public {
+        require(balanceReceived[msg.sender] >= _amount, "Not enough funds");
+        balanceReceived[msg.sender] -= _amount;
+        _to.transfer(_amount);
+    }
 
     function withdrawAllMoney(address payable _to) public {
-        _to.transfer(address(this).balance);
+        uint256 balanceToSend = balanceReceived[msg.sender];
+        balanceReceived[msg.sender] = 0;
+        _to.transfer(balanceToSend);
     }
 }
