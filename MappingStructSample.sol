a557bd21a622f482c3db9ffa8d0dfe4f4ef5d140
ksym04 <dev@dopethemes.com>
2022-07-03 23:34:12 +0800

    Fix mapping struct.

--- MappingStructSample.sol
+++ MappingStructSample.sol
@@ -1,26 +1,49 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.1;
+
+pragma solidity ^0.8.7;
 
 contract MappingStructSample {
-    mapping(address => uint256) public balanceReceived;
+    struct Payment {
+        uint256 amount;
+        uint256 timestamps;
+    }
+
+    struct Balance {
+        uint256 totalBalance;
+        uint256 numPayments;
+        mapping(uint256 => Payment) payments;
+    }
+
+    // Map of balances.
+    mapping(address => Balance) public balanceReceived;
 
     function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
 
     function sendMoney() public payable {
-        balanceReceived[msg.sender] += msg.value;
+        balanceReceived[msg.sender].totalBalance += msg.value;
+
+        Payment memory payment = Payment(msg.value, block.timestamp);
+
+        balanceReceived[msg.sender].payments[
+            balanceReceived[msg.sender].numPayments
+        ] = payment;
+        balanceReceived[msg.sender].numPayments++;
     }
 
     function withdrawMoney(address payable _to, uint256 _amount) public {
-        require(balanceReceived[msg.sender] >= _amount, "Not enough funds");
-        balanceReceived[msg.sender] -= _amount;
+        require(
+            balanceReceived[msg.sender].totalBalance >= _amount,
+            "Not enough funds"
+        );
+        balanceReceived[msg.sender].totalBalance -= _amount;
         _to.transfer(_amount);
     }
 
     function withdrawAllMoney(address payable _to) public {
-        uint256 balanceToSend = balanceReceived[msg.sender];
-        balanceReceived[msg.sender] = 0;
+        uint256 balanceToSend = balanceReceived[msg.sender].totalBalance;
+        balanceReceived[msg.sender].totalBalance = 0;
         _to.transfer(balanceToSend);
     }
 }
